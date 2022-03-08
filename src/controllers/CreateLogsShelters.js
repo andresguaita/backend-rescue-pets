833ff68716aca65359e4c5ddfbdc5b979e0b8d21
AilenMz <ailen.martinez@usal.edu.ar>
2022-03-08 15:50:26 -0300

    agregar ruta :P

--- src/controllers/CreateLogsShelters.js
+++ src/controllers/CreateLogsShelters.js
@@ -6,103 +6,104 @@ const { generateJWT } = require("../../helpers/jwt");
 var sequelize = require("sequelize");
 const { transporter } = require("../utils/configNodemailer");
 async function createShelter(req, res) {
+  try {
+    const {
+      name,
+      phoneNumber,
+      description,
+      address,
+      email,
+      password,
+      cityId,
+      role,
+      img,
+    } = req.body;
 
-    try {
-        const { name, phoneNumber, description, address, email, password ,cityId,  role, img } = req.body;
-      
+    const errors = validationResult(req);
 
-        const errors= validationResult(req)
-
-        if(!errors.isEmpty()){
-            return res.status(400).json({
-                ok: false,
-                errors : errors.mapped()
-            })
-        }
-
-        const salt = bcrypt.genSaltSync()
-
-       
-        const hash = bcrypt.hashSync(password, salt)
-
-        const User =  await Users.create({
-             email: email,
-             password : hash,
-             isVerified: false,
-             roleId: role
-         });
-
-
-         const token = await generateJWT(User.id, User.email)
-
-        const createShelter = await Shelter.create({
-            name, address, phoneNumber, description, userId : User.id , cityId, img
-        });
-
-        const urlConfirm = `${process.env.APIGATEWAY_URL}/confirm/${token}`
-
-        await transporter.sendMail({
-          from: process.env.EMAIL,
-          to: User.email,
-          subject: "Por favor confirme su cuenta",
-          html: `<p>Por favor confirme su cuenta <a href="${urlConfirm}">Confirmar</a></p>`
-        })
-       
-        res.status(201).send({
-            ok: true,
-            createShelter,
-            id: User.id,
-            email: User.email,
-            token
-         });
-
-    } catch (error) {
-        res.json('Error en el Catch.');
-        console.log(error);
-
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        ok: false,
+        errors: errors.mapped(),
+      });
     }
 
-  } 
-  const getAllShelters = async () => {
-    return await Shelter.findAll({  
-      include: {
-          model: Cities,
-          include: {
-              model: States,
-              include: Countries
-          }
-        }
-       });
-    };
+    const salt = bcrypt.genSaltSync();
 
-// const getAllSheltersPaises = async () => {
-//    return await Shelter.findAll({  
-//        include: {
-//         model: Cities,
-//         include: {
-//             model: States,
-//             attributes: ["countryId"],
-//             include: {model: Countries, attributes:["id","country"]} 
-//         },
-//          attributes:["stateId"],
-//       }, attributes:[]
-//      });
-//   };
+    const hash = bcrypt.hashSync(password, salt);
+
+    const User = await Users.create({
+      email: email,
+      password: hash,
+      isVerified: false,
+      roleId: role,
+    });
+
+    const token = await generateJWT(User.id, User.email);
+
+    const createShelter = await Shelter.create({
+      name,
+      address,
+      phoneNumber,
+      description,
+      userId: User.id,
+      cityId,
+      img,
+    });
+
+    const urlConfirm = `${process.env.APIGATEWAY_URL}/confirm/${token}`;
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to: User.email,
+      subject: "Por favor confirme su cuenta",
+      html: `<p>Por favor confirme su cuenta <a href="${urlConfirm}">Confirmar</a></p>`,
+    });
+
+    res.status(201).send({
+      ok: true,
+      createShelter,
+      id: User.id,
+      email: User.email,
+      token,
+    });
+  } catch (error) {
+    res.json("Error en el Catch.");
+    console.log(error);
+  }
+}
+
+const getAllShelters = async () => {
+  return await Shelter.findAll({
+    include: [
+      {
+        model: Cities,
+        include: {
+          model: States,
+          include: Countries,
+        },
+      },
+      {
+        model: Users,
+        attributes: ["email"],
+      },
+    ],
+  });
+};
 
 const getAllSheltersPaises = async () => {
-    return await Shelter.findAll({  
-        include: {
-         model: Cities,
-         include: {
-             model: States,
-             attributes: ["countryId"],
-             include: {model: Countries, attributes:["id","country"]} 
-         },
-          attributes:["stateId"],
-       }, attributes:["id","name"]
-      });
-   };
-
-  
+  return await Shelter.findAll({
+    include: {
+      model: Cities,
+      include: {
+        model: States,
+        attributes: ["countryId"],
+        include: { model: Countries, attributes: ["id", "country"] },
+      },
+      attributes: ["stateId"],
+    },
+    attributes: ["id", "name"],
+  });
+};
 
 module.exports = { createShelter, getAllShelters, getAllSheltersPaises };
