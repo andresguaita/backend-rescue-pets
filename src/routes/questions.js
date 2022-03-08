0eba074d97910fa271eaf0e10259a4f798f20649
AilenMz <ailen.martinez@usal.edu.ar>
2022-03-08 04:21:27 -0300

    crud preguntas

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getAllQuestions} = require('../controllers/getAllQuestions')
+const {getAllQuestions, postSupport, putQuestions} = require('../controllers/getAllQuestions')
 
 router.get('/getAllQuestions', getAllQuestions)
+
+router.post('/postQuestions', postSupport)
+
+router.put('/putQuestions/:idquestion', putQuestions);
 
 module.exports = router
