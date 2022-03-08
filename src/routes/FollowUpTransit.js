f74ba0a7784788bf127edea7445fdcfe0786297f
alexisonfiremx <themarkvolta@gmail.com>
2022-03-07 21:38:17 -0800

    creacion de ruta editPetInTransit

--- src/routes/FollowUpTransit.js
+++ src/routes/FollowUpTransit.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const {addFollowUpTransit} = require('../controllers/addFollowUpTransit')
 const {getFollowUpTransits} = require('../controllers/getFollowUpTransit')
 const {editFollowUpTransit} = require('../controllers/editFollowUpTransit')
+const {editPetsAssigned} = require('../controllers/editPetsAssigned')
 
 router.post('/addFollowUpTransit', addFollowUpTransit );
 router.get('/followUpTransit/:shelterId', getFollowUpTransits);
 router.put('/followUpTransit/:editableTransitId', editFollowUpTransit);
+router.put('/followUpTransit/petsAssigned/:editableTransitId', editPetsAssigned);
 
 module.exports = router
