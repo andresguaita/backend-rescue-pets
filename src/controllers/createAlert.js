35c4616909a361206c6d0b2416952de362b7e3d9
[oriirigo] <[orii.irigo@gmail.com]>
2022-03-09 05:01:10 -0300

    back: cambio alert

--- src/controllers/createAlert.js
+++ src/controllers/createAlert.js
@@ -4,7 +4,7 @@ const {Alerts} = require('../db.js')
 exports.createAlert = async (req,res) => {
     const {image,direction,description,shelterId} = req.body
     try{
-if(direction && description & shelterId &&image){
+
     let ca= await Alerts.create({
         direction:direction,
         image:image,
@@ -16,9 +16,7 @@ if(direction && description & shelterId &&image){
         ok:true,
         ca
     })
-} else{
-            return res.status(400).send('wrong data')
-        }
+
     }catch(error){
         return res.status(500).send('Error connecting to the database')
     }    
