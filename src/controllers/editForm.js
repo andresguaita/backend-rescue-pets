edcf1bcfcff51dc1936a4382a9b818881204f79c
Julian-Lop <you@example.com>
2022-03-09 18:18:59 -0500

    Back-bug-resolved-route-editform-julian

--- src/controllers/editForm.js
+++ src/controllers/editForm.js
@@ -6,41 +6,22 @@ exports.editForm = async(req,res)=>{
         const {formtypeid} = req.query
         const {questions} = req.body
         try{
-            if(formid && formtypeid){
-                let formtype = await Formtype.findOne({
-                    where:{
-                        id : formtypeid
-                    }
-                })
-    
-                let shelterid = await Forms.findOne({
+            if(formid){
+                let form = await Forms.findOne({
                     where:{
                         id: formid,
                     }
                 })
-                
-                let temp1 = await Forms.destroy({
-                    where:{
-                        id: formid,
-                    }
-                })
-    
-                let temp2 = await Forms.create({
-                    id : formid,
-                    formName : formtype.typeName,
-                    formtypeId : formtypeid,
-                    shelterId : shelterid.shelterId
-                })
-    
                 questions.map(async question => {
                         let q = await Questions.findOne({
                             where:{
                                 id : question
                             }
                         })
-                        temp2.addQuestions(q)
+                        await form.setQuestions(q)
+                        await form.addQuestions(q)
                 })
-                return res.status(200).json(temp2) 
+                return res.status(200).json(form) 
             }else{
                 return res.status(400).send('wrong data')
             }
