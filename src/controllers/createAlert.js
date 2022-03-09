c2491eacadda74aa3784cb5fb2eea49ed1239740
[oriirigo] <[orii.irigo@gmail.com]>
2022-03-09 05:36:20 -0300

    back: email en alert

--- src/controllers/createAlert.js
+++ src/controllers/createAlert.js
@@ -1,10 +1,13 @@
-const {Alerts} = require('../db.js')
-
+const {Alerts} = require('../db.js');
+const {Users}=require('../db.js')
 
 exports.createAlert = async (req,res) => {
     const {image,direction,description,shelterId} = req.body
     try{
+let User=await Users.findOne({
+where:{shelterId:shelterId}
 
+})
     let ca= await Alerts.create({
         direction:direction,
         image:image,
@@ -12,12 +15,18 @@ exports.createAlert = async (req,res) => {
         shelterId:shelterId
        
     })
-     return res.json({
+    await transporter.sendMail({
+        from: `"Rescue Pets" <${EMAIL}> `,
+        to: User.email,
+        subject: `Nueva alerta de rescate`,
+        html: `<p>Un usuario ha emitido una alerta de rescate cercana a tu refugio, por favor ingrese al  panel de administración, y verifique su alerta</p>`,
+        
+    })
+    return res.json({
         ok:true,
         ca
     })
-
-    }catch(error){
+}   catch(error){
         return res.status(500).send('Error connecting to the database')
     }    
 }
